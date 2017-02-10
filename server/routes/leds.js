840a2b77414b494cedbc81ac72d532d89c747f4d
Marijn Willemse <willemse.marijn@gmail.com>
2017-02-10 14:59:06 +0100

    Led all the things!

--- server/routes/leds.js
+++ server/routes/leds.js
@@ -3,11 +3,17 @@ var express = require('express');
 var debug = require('debug')('artof-leds');
 var router = express.Router();
 
+var five = require("johnny-five");
+var board = new five.Board();
+
 router.get( '/leds/:nr', function ( httpRequest, httpResponse ) {
 
     if ( httpRequest.params.nr ) {
 
         httpResponse.send( 'We gaan ledje '+ httpRequest.params.nr +' aanzetten' );
+		
+		myLed = new five.Led(httpRequest.params.nr);
+		myLed.off();		
 
     } else {
         httpResponse.status( 400 );
